05e47930a389a082ded71aefc56b5887a8153544
Hugo Serrat <hu.serrat@gmail.com>
2024-11-15 14:21:27 +0000

    feat: support for linux-yocto as kernel provider

--- external/openembedded-layer/recipes-support/libusbgx/libusbgx-config/l4t-gadget-config-setup.sh
+++ external/openembedded-layer/recipes-support/libusbgx/libusbgx-config/l4t-gadget-config-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-modprobe libcomposite bridge
+[ -d /sys/module/libcomposite ] || modprobe libcomposite
+[ -d /sys/module/bridge ] || modprobe bridge
+
 [ -d /run/usbgx ] || mkdir /run/usbgx
 [ ! -e /run/usbgx/l4t.schema ] || exit 0
 if [ ! -e /usr/share/usbgx/l4t.schema.in ]; then
@@ -8,5 +10,5 @@ if [ ! -e /usr/share/usbgx/l4t.schema.in ]; then
 fi
 sernum=$(cat /proc/device-tree/serial-number 2>/dev/null | tr -d '\000')
 [ -n "$sernum" ] || sernum="UNKNOWN"
-sed -e"s,@SERIALNUMBER@,$sernum," /usr/share/usbgx/l4t.schema.in > /run/usbgx/l4t.schema
+sed -e"s,@SERIALNUMBER@,$sernum," /usr/share/usbgx/l4t.schema.in >/run/usbgx/l4t.schema
 chmod 0644 /run/usbgx/l4t.schema
